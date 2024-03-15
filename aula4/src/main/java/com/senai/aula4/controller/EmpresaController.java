706c603cc6fcde8b38f64ebcc721a3973ce3d372
Andre <andresnu@tutanota.com>
2024-03-14 22:21:36 -0300

    atualiza aula 3

--- aula4/src/main/java/com/senai/aula4/controller/EmpresaController.java
+++ aula4/src/main/java/com/senai/aula4/controller/EmpresaController.java
@@ -2,7 +2,6 @@ package com.senai.aula4.controller;
 
 import com.senai.aula4.entites.EmpresaEntity;
 import com.senai.aula4.service.EmpresaService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,10 +9,13 @@ import java.util.List;
 @RestController // indica um endpoint que trabalha com JSON, ou seja um endpoint REST
 // adiciona o Controller a memória do Spring Boot, como um Bean
 @RequestMapping("/empresa") //indica o caminho dos recursos dessa Classe
-@RequiredArgsConstructor
 public class EmpresaController {
 
     private final EmpresaService empresaService;
+
+    public EmpresaController(EmpresaService empresaService) {
+        this.empresaService = empresaService;
+    }
 
     @GetMapping //mapeia o método GET do REST, esse retorna dados
     public List<EmpresaEntity> retornarEmpresas() {
--- aula4/src/main/java/com/senai/aula4/service/EmpresaService.java
+++ aula4/src/main/java/com/senai/aula4/service/EmpresaService.java
@@ -2,11 +2,9 @@ package com.senai.aula4.service;
 
 import com.senai.aula4.entites.EmpresaEntity;
 import com.senai.aula4.repository.EmpresaRepository;
-import com.senai.aula4.repository.FuncionarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
